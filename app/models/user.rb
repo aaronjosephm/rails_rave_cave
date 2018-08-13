2363c2c1eeb2f81aa4c1b7a6f25ea5487f7cdbc7
Aaron Mednick <aaronjosephm@gmail.com>
2018-08-13 16:03:27 -0400

    fixed user cabling

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :booked_warehouses, through: :bookings, class_name: "Warehouse"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :profile, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
