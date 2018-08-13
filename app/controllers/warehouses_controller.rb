16f04a8be809a3627aa70c6ae5a9ef39f8e27e32
furkan karayer <furkankarayer@gmail.com>
2018-08-13 16:40:40 -0400

    controller for warehouses finished

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -8,14 +8,34 @@ class WarehousesController < ApplicationController
   end
 
   def new
+    @warehouse = Warehouse.new
   end
 
   def create
+    @warehouse = Warehouse.new(warehouse_params)
+
+    if @warehouse.save
+      redirect_to warehouses_path
+    else
+      render :new
+    end
   end
 
   def update
+    if @warehouse.update(warehouse_params)
+      redirect_to warehouse_path(@warehouse)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @warehouse = Warehouse.find(params[:id])
+    @warehouse.destroy
+    redirect_to warehouses_path
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :photo, :description, :user_id)
   end
 end
