12b84040552f37ba26b4ae58322c5fdf0f225663
Caroline Shearer <carolineshearer88@gmail.com>
2018-08-13 16:11:02 -0400

    Finished bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,36 @@
 class BookingsController < ApplicationController
+
+  # received bookings are the bookings that the creator of the warehouse receive
   def received
   end
 
+  # these are the bookings that the user has made for him/her.
   def sent
+    @user = User.find(params[:user_id])
+    @bookings = @user.bookings
   end
 
   def warehouse_bookings_index
+    @bookings = Booking.all
   end
 
   def new
+    @bookings = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = User.find(params[:user_id])
+    @booking.warehouse = Warehouse.find(params[:warehouse_id])
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
   end
 
   def destroy
+    @booking.destroy
+    redirect_to bookings_path
   end
 end
+
