7d43cd4644aefb150ab5da3d7758f3f27dd1a540
Caroline Shearer <carolineshearer88@gmail.com>
2018-08-15 15:58:05 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,30 +11,30 @@ caroline = User.create(email: "scottish@yahoo.com", password: "braveheart")
 
 turkey = Warehouse.new(name: "Hairy Addiction",
                         description: "Raki All Day Long",
-                        photo: "https://d31fr2pwly4c4s.cloudfront.net/7/5/7/938101_0_warehouseraves_400.jpg"
+                        photo: "https://images.unsplash.com/photo-1509318754830-684cc505913d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4559482dc487df0f9b7ec31f24bf8213&auto=format&fit=crop&w=1050&q=80.jpg"
                         )
 
 turkey.user = caroline
 turkey.save
 
 turkish_bath_house = Warehouse.new(name: "Tiger in the Bathroom",
-                        description: "Hot, Sweaty exfoliation",
+                        description: "WOOOOOOW",
                         photo: "http://unikevent.ca/wp-content/uploads/2016/12/Fonderie-Darling-1170x779.jpg"
                         )
 
 turkish_bath_house.user = mark
 turkish_bath_house.save
 
-schmuck_house = Warehouse.new(name: "Schmuck house",
+schmuck_house = Warehouse.new(name: "Schmuck House",
                         description: "Stomping the Glass All Day Long",
-                        photo: "https://martell-production.s3.amazonaws.com/uploads/uploaded_file/filename/272549/large_Warehouse_To-let___32_Montreal_Drive___Airport_City__Cape_Town____1_.JPG"
+                        photo: "https://images.unsplash.com/photo-1519675859442-f3bb5398b12a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=629437bc173e92e66e4380fab0080b0a&auto=format&fit=crop&w=1500&q=80.JPG"
                         )
 
 schmuck_house.user = caroline
 schmuck_house.save
 
-mile_end = Warehouse.new(name: "Falafel Sweetness",
-                       description: "Fine Tuned Pump Action",
+mile_end = Warehouse.new(name: "Eternal Snow",
+                       description: "Parking Lot Nose Job",
                        photo: "https://cdn.decoist.com/wp-content/uploads/2015/02/Caviar-warehouse-converted-into-a-contemporary-loft-in-New-York.jpg"
                        )
 
@@ -51,7 +51,7 @@ under_table_stool.save
 
 scotish_cave = Warehouse.new(name: "Radical",
                              description: "Feel the Berne",
-                             photo: "http://www.real-estate-vietnam.com/images/bien607/p1.jpg"
+                             photo: "https://d31fr2pwly4c4s.cloudfront.net/7/5/7/938101_0_warehouseraves_400.jpg"
                              )
 scotish_cave.user = caroline
 scotish_cave.save
@@ -66,8 +66,8 @@ crack_house.user = mark
 crack_house.save
 
 
-snow = Warehouse.new(name: "Eternal Snow",
-                        description: "Parking Lot Nose Job",
+snow = Warehouse.new(name: "Turkish Bath House",
+                        description: "Hot, Sweaty exfoliation",
                         photo: "https://d31fr2pwly4c4s.cloudfront.net/7/5/7/938101_0_warehouseraves_400.jpg"
                         )
 
