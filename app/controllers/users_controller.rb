eb741ae855809fc7f8a085d8a29e64014d8c5cd9
Caroline Shearer <carolineshearer88@gmail.com>
2018-08-16 18:16:16 -0400

    Updated profile page and navbar links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,8 @@ class UsersController < ApplicationController
     @warehouses = current_user.warehouses
     @bookings = current_user.bookings
   end
-<<<<<<< HEAD
 
   def warehouses
-
-=======
-
-  def warehouses
->>>>>>> e071de9f3aefccc965f83f3fc76c6e54dc23f7cf
     authorize current_user
     @warehouses = current_user.warehouses
     @bookings = current_user.bookings
